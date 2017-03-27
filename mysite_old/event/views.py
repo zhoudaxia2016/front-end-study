b59c8e7ea5f83b07162151cd10cc9795c25f675d
mzhou <1207190489@qq.com>
2017-03-27 09:57:55 +0800

    将mysite移动到新的独立的仓库

--- mysite_old/event/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Event
-
-# Create your views here.
-def event(rq):
-    events = Event.objects.all()
-    rp = render(rq,'event/event.html',{'events': events})
-    rp.set_cookie('page','3');
-    return rp
--- mysite_old/remark/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url 
-from . import views
-
-app_name = 'login'
-
-urlpatterns = [
-    url(r'^blog/?$', views.blogRemark,name='blogRemark'),
-    url(r'^demo/?$', views.demoRemark,name='demoRemark'),
-]
